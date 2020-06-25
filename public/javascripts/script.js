41eca591d3845b3a0fafdb9c621033e1e5db75cb
Joel Rudin <jarud55@gmail.com>
2020-06-25 11:19:27 -0400

    html tweak

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -60,7 +60,7 @@ function setTime() {
   $('#intro').empty();
   $('#srtBtn').empty();
   displyField.innerHTML =
-    "<input type='number' name='result2' id='result2' class='clearField' style='width: 35px;' required min='1' max='3'> ";
+    "<input type='number' name='result2' id='result2' class='clearField' style='width: 35px;' required min='1' max='6'> ";
   event.preventDefault();
   finalScore = document.getElementById('result2');
   var userChoice;
