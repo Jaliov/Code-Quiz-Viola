0f9ab0e8c5c13d6b4ce3f6b8b0eaf135097d3e87
Joel Rudin <jarud55@gmail.com>
2020-06-26 10:57:48 -0400

    tweaks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+// require('dotenv').config();
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,14 +9,15 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(express.static(__dirname + 'public/stylesheets/style.css'));
 app.use(express.static(__dirname + 'public/javascripts/script.js'));
 
+var mongoose = require('mongoose');
 const dbName = 'test';
 
-var url =
+var MONGODB_URI =
   'mongodb+srv://TestViola:Flunky5832@cluster0-sils7.mongodb.net/test?retryWrites=true&w=majority';
-var mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  url || 'mongodb://localhost/Code-Quiz-Viola',
+  MONGODB_URI || 'mongodb://localhost:27017/Code-Quiz-Viola',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -43,7 +44,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/addinitials', (req, res) => {
+app.post('/addInits', (req, res) => {
   var myData = new User(req.body);
   myData.save().catch((err) => {
     res.status(400).send('unable to save to database');
